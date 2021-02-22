7e63289547018327550a5830740dc05aa15bacba
kelbling <matthias.kelbling@ufz.de>
2021-02-22 13:43:48 +0100

    adjusted paths in control_file

--- test_run/control_file.py
+++ test_run/control_file.py
@@ -1,14 +1,33 @@
-path_grdc_data = "/data/esm/global_mod/data/processed/GRDC_time_series/"
-path_grdc_alloc = "/work-local/yadegari/optim_Eve/ecfpy/suites/htcal/luts"
+# --------------------------------------------------
+#  -- USER SETTINGS --------------------------------
+# --------------------------------------------------
+#  -- path to git repo 'optim' ---------------------
+# path_aux_dir = "/work-local/yadegari/optim_Eve/aux"
+path_aux_dir = "/work-local/kelbling/optim"
+#  -- path to exe dir ------------------------------
+# TODO: change path to mpr exe, to e.g. mpr_{mpr_tf}
+# expects to contain:
+# htessel exe, as: master1s.exe
+# mpr exectuables in the subdir:
+# mpr/{mpr_tf}/MPR-0.6.7 ({mpr_tf} is e.g. 'zacharias')
+# path_execs = "/work-local/yadegari/optim_Eve/execs"
+path_execs = "/work-local/kelbling/optim_htcal_exes"
 
-path_forcing = "/work-local/yadegari/optim_Eve/forcings"
+# --------------------------------------------------
+#  -- GLOBAL SETTINGS ------------------------------
+# --------------------------------------------------
+#  -- cluster specific paths -----------------------
+path_grdc_data = "/data/esm/global_mod/data/processed/GRDC_time_series"
+path_grdc_alloc = "/data/htcal/data/processed/luts"
+
+path_forcing = "/data/htcal/data/processed//forcings"
 path_static = "/data/htcal/data/processed/static/15min/ini_data"
 
-path_aux_dir = "/work-local/yadegari/optim_Eve/aux"
-path_soilgrid = "/work-local/yadegari/soilgrids"
+path_soilgrid = "/data/htcal/data/processed/mpr_in_data"
 
-path_execs = "/work-local/yadegari/optim_Eve/execs"
-
+# --------------------------------------------------
+#  -- EXPERIMENT SETTINGS --------------------------
+# --------------------------------------------------
 mpr_tf = 'zacharias'
 
 training = {
