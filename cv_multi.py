b0382f9593362c440abc3d5fffb8c712ad6e1e65
Sourena Yadegari Varnamkhasti <sourena.yadegari@ufz.de>
2022-06-02 16:13:15 +0200

    cv_multi tested on Eve and working

--- cv_multi.py
+++ cv_multi.py
@@ -12,12 +12,14 @@ import subprocess as sp
 import copy
 import pprint
 import numpy as np
+import pathlib
 
 from squash_multiyear import get_info
 from lutreader import basin_lut
 from modify_sim import modify_sim_param
-from util import import_control_file
+from util import import_control_file, get_station_number, get_optim_path
 
+DEBUG = True
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -53,7 +55,141 @@ def parse_arguments():
                     With "default" uses the executable that is defined in htcal_path.\n \
                         Only works when --squash-sim is enabled'
                     )
+    parser.add_argument('--calculate-kge',
+                    nargs='?', const=True, default=False,
+                    help='Calculates kge'
+                    )
     return parser.parse_args()    
+
+
+def run_shell_cmd(cmd, cwd):
+    out, err = sp.Popen(cmd,
+                        shell=True,
+                        cwd=cwd,
+                        stdout=sp.PIPE,
+                        stderr=sp.PIPE).communicate()
+    return out, err
+
+
+def create_control_file(cv_info, control_file_path):
+    st_num = get_station_number(cv_info['station'])
+    
+    training = \
+    {
+        st_num:
+            {
+                'year_begin': cv_info['year_begin'],
+                'year_end': cv_info['year_end'],
+                'warmup': cv_info['warmup']
+            }
+    }
+
+    cf, _, _ = import_control_file(control_file_path)
+    names = [name for name in dir(cf) if not name.startswith('__')]
+    with open('control.py', 'w') as f:
+        pp = pprint.PrettyPrinter(indent=4, stream=f)
+        # first print training
+        print('training= \\', file=f)
+        pp.pprint(training)
+        print('', file=f)
+        # iterate on the rest
+        for name in list(set(names) - {'training'}):
+            print(f'{name} = \\', file=f)
+            pp.pprint(getattr(cf, name))
+            print('', file=f)
+
+
+def create_default_sim(cv_info:dict, 
+                       control_file_path:str):
+    '''
+    TODO: draw tree output for documentation here
+    '''
+    # we should be in CV/station_123456
+    create_control_file(cv_info, control_file_path)
+        
+    cmd = ' '.join(
+        [
+            'python',
+            f'{get_optim_path()}/prepare_domains.py',
+            '-c control.py',
+            f'-l {get_optim_path()}/basin_lut.org'
+        ]
+    )
+    cwd = '.'
+    _, _ = run_shell_cmd(cmd, cwd)
+
+
+def modify_params(station, param_set):
+    with Path(f'default_sim/{station}'):
+        modify_sim_param(param_set)
+
+
+def calculate_and_write_kge():
+    ...
+
+
+def gather_kge():
+    ...
+
+
+def run_squash(disable_wb_check,
+               htessel_exec):
+    cmd = ' '.join(
+        [
+            'python',
+            f'{get_optim_path()}/squash_multiyear.py',
+            '-c control.py',
+            f'-l {get_optim_path()}/basin_lut.org',
+            '-q',
+            (lambda: '--disable-wbcheck' if disable_wb_check else '')(),
+            f'--htessel-exec {htessel_exec}'
+        ]
+    )
+    cwd = '.'
+    _, _ = run_shell_cmd(cmd, cwd)
+
+
+def run_mpr(name):
+    cmd = './run_mpr'
+    cwd = f'default_sim/{name}'
+    _, _ = run_shell_cmd(cmd, cwd)
+
+
+def run_htessel(name):
+    cmd = './run_htessel'
+    cwd = f'default_sim/{name}'
+    _, _ = run_shell_cmd(cmd, cwd)
+
+
+def worker_fn(cv_info:dict,
+              cf_address: str,
+              param_set:dict,
+              squash_sim:bool,
+              disable_wb_check:bool,
+              htessel_exec:str,
+              calculate_kge:bool) -> None:
+    '''
+    cv_info: information we need to construct cross validation station
+             e.g.: {'station': 'station_123456',
+                    'year_begin': 1988,
+                    'year_end': 1992,
+                    'warmup': 365}
+    cf_address: address of the control_file we used for optimization.
+                we use this file together with cv_info to construct
+                a new control file for the station in validation set. 
+                (include filename and extension)
+    param_set: best param set from optimization
+    '''
+    with Path(cv_info['station']).mkdir():
+        if DEBUG: print(f'in worker: {cv_info}')
+        create_default_sim(cv_info, cf_address)
+        modify_params(cv_info['station'], param_set)
+        if squash_sim:
+            run_squash(disable_wb_check, htessel_exec)
+        run_mpr(cv_info['station'])
+        run_htessel(cv_info['station'])
+        if calculate_kge:
+            calculate_and_write_kge()
 
 
 def parse_params(param_str:str, cf_params:dict) -> dict:
@@ -75,160 +211,27 @@ def get_best_param_set(res_file_lines:list, cf_params:dict) -> Tuple[float, int,
                 max_kge = kge
                 best_param_set = parse_params(params, cf_params)
                 best_iter_num = idx
-
+    #
     return max_kge, best_iter_num, best_param_set
 
 
-def run_shell_cmd(cmd, cwd):
-    out, err = sp.Popen(cmd,
-                        shell=True,
-                        cwd=cwd,
-                        stdout=sp.PIPE,
-                        stderr=sp.PIPE).communicate()
-    return out, err
-
-
-def create_control_file(cf, 
-                        station_number:str,
-                        year_begin:int,
-                        year_end:int,
-                        warmup:int):
-    
-    training = {station_number: {
-        'year_begin': year_begin,
-        'year_end': year_end,
-        'warmup': warmup
-    }}
-
-    names = [name for name in dir(cf) if not name.startswith('__')]
-    
-    with open('control.py', 'w') as f:
-        pp = pprint.PrettyPrinter(indent=4, stream=f)
-        # first print training
-        print('training= \\', file=f)
-        pp.pprint(training)
-        print('', file=f)
-        # iterate on the rest
-        for name in list(set(names) - set('training')):
-            print(f'{name} = \\', file=f)
-            pp.pprint(getattr(cf, name))
-            print('', file=f)
-    
-
-
-def create_default_sim(cf, name):
-    '''
-    CV
-    ├──-- station_N
-         ├──  control.py   ├── default   
-    '''
-    with Path('XXX'): # we should be in CV/station_123456
-        
-        create_control_file(cf)
-        
-        cmd = ' '.join(
-            [
-                'prepare_domain.py',
-                '-c control.py',
-                '-l XXX'
-            ]
-        )
-        cwd = '.'
-        _, _ = run_shell_cmd(cmd, cwd)
-
-
-def modify_params(name, param_set):
-    with Path('XXX'):
-        modify_sim_param(param_set)
-
-
-def run_squash(name,
-               disable_wb_check,
-               htessel_exec):
-    cmd = ' '.join(
-        [
-            'squash_multiyear.py',
-            '-c control.py',
-            '-l XXX',
-            '-q',
-            (lambda: '--disable-wbcheck' if disable_wb_check else '')(),
-            f'--htessel-exec {htessel_exec}'
-        ]
-    )
-    cwd = ''
-    _, _ = run_shell_cmd(cmd, cwd)
-
-
-def run_mpr(name):
-    cmd = 'run_mpr'
-    cwd = ''
-    _, _ = run_shell_cmd(cmd, cwd)
-
-
-def run_htessel(name):
-    cmd = 'run_htessel'
-    cwd = ''
-    _, _ = run_shell_cmd(cmd, cwd)
-
-
-def worker_fn(cv_info:dict,               # eg: {'station': 'station_123456', 'year_begin':1988, 'year_end':1992}
-              cf_address: str,            # address of control file used in optiimization
-              param_set:dict,
-              squash_sim:bool,
-              disable_wb_check:bool,
-              htessel_exec:str) -> None:
-    '''
-    cv_info: information we need to construct cross validation station
-             e.g.: 'station': 'station_123456', 'year_begin':1988, 'year_end':1992} 
-    cf_address: address of the control_file we used for optimization.
-                we use this file together with cv_info to construct
-                a new control file for the station in validation set. 
-                (include filename and extension)
-    param_set: best param set from optimization
-    '''
-    
-    with Path(cv_info['station']).mkdir():
-        create_default_sim(cv_info, cf_address)
-        modify_params(cv_name, param_set)
-        if squash_sim:
-            run_squash(cv_name, disable_wb_check, htessel_exec)
-        run_mpr(cv_name)
-        run_htessel(cv_name)
-
-
-def main():
-
-    args = parse_arguments()
-    cf, path_root, cf_name = import_control_file(args.control_file)
-
+def create_cv_folder(cv_folder_path:str) -> None:
     # use the folder if it exists
     if not os.path.exists(f'{args.cv_folder_path}/CV/'):
         os.makedirs(f'{args.cv_folder_path}/CV/',)
     # path_root_abs = os.path.abspath(path_root)
 
-    grdcs = get_info(cf, basin_lut(args.basin_lut))
-    run_dirs = {grdc.run_dir: (grdc.year_begin, grdc.year_end) for grdc in grdcs}
-    print(run_dirs.keys())
 
-    with open(args.cv_stations, 'r') as f:
-        cvs = json.load(f)
-    # we use these for CV
-    cv_ = list(set(cvs) - set(run_dirs))
-    cv_ = [
-        {'station': st,
-         'year_begin': cvs[st]['yb'][0],
-         'year_end': cvs[st]['yb'][0] + cvs[st]['nyrs'][0] - 1
-        } for st in cv_
-    ]
-    #
-
+def write_best_param_set(path_root:str,
+                          cv_folder_path:str,
+                          optim_params:dict) -> None:
     # read res.txt
-    with open(f'{path_root}/runs/res.txt')as f:
+    with open(f'{path_root}/runs/res.txt') as f:
         lines = f.readlines()
-
-    max_kge, best_iter_num, best_param_set = get_best_param_set(lines, cf.params)
-
-    with open(f'{args.cv_folder_path}/CV/best_param_set.json', 'w') as f:
+    #
+    max_kge, best_iter_num, best_param_set = get_best_param_set(lines, optim_params)
+    #
+    with open(f'{cv_folder_path}/CV/best_param_set.json', 'w') as f:
         json.dump(
             {
                 'simulation': path_root,
@@ -240,18 +243,54 @@ def main():
             indent=4
         )
 
+
+def main():
+
+    args = parse_arguments()
+
+    cf, path_root, cf_name = import_control_file(args.control_file)
+
+    create_cv_folder(args.cv_folder_path)
+
+    grdcs = get_info(cf, basin_lut(args.basin_lut))
+    run_dirs = {grdc.run_dir: (grdc.year_begin, grdc.year_end) for grdc in grdcs}
+
+    #
+    with open(args.cv_stations, 'r') as f:
+        cvs = json.load(f)
+    # we use these for CV
+    cv_ = list(set(cvs) - set(run_dirs))
+    cv_ = [
+        {'station': st,
+         'year_begin': cvs[st]['yb'][0],
+         'year_end': cvs[st]['yb'][0] + cvs[st]['nyrs'][0] - 1,
+         'warmup': args.warmup
+        } for st in cv_
+    ]
+    #
+    write_best_param_set(path_root,
+                         args.cv_folder_path,
+                         cf.params)
+    #
+    path_worker = copy.deepcopy(sys.path)
+    path_worker.insert(0, get_optim_path())
+    #
     with Path(f'{args.cv_folder_path}/CV'):
-        with futures.MPIPoolExecutor() as pool:
+        with futures.MPIPoolExecutor(path=path_worker) as pool:
             pool.starmap(
                 worker_fn,
                 zip(cv_,
                     repeat(f'{path_root}/{cf_name}.py', len(cv_)),
                     repeat(best_param_set, len(cv_)),
                     repeat(args.squash_sim, len(cv_)),
-                    repeat(args.disable_wbcheck_param, len(cv_)),
-                    repeat(args.htessel_exec, len(cv_))
+                    repeat(args.disable_wbcheck, len(cv_)),
+                    repeat(args.htessel_exec, len(cv_)),
+                    repeat(args.calculate_kge, len(cv_))
                     )
             )
+    #
+    if args.calculate_kge:
+        gather_kge()
 
 
 if __name__ == '__main__':
