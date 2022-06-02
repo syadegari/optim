1141fd448ab4f4d599df9b79121afa367f44d7e0
Sourena Yadegari Varnamkhasti <sourena.yadegari@ufz.de>
2022-06-02 16:01:08 +0200

    added dunder method
    To facilitate search in objects that are inheriting from the base.

--- abstract_namelist.py
+++ abstract_namelist.py
@@ -20,9 +20,16 @@ class AbstractNameList(ABC):
     def __find__(self, kw):
         raise NotImplemented
 
+    def __contains__(self, kw):
+        try:
+            _ = self.__find__(kw)
+            return True
+        except:
+            return False
+
     @abstractmethod
     def get_all_model_parameters(self) -> List[str]:
         raise NotImplemented
 
-    def write(self, path='.'):
+    def write(self, path='.'):   #TODO: remove default path and check the code 
         self.nml.write(f"{path}/{self.tag}", force=True)
