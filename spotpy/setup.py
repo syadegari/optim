01e77823d803fdc1970a8ca998b763fc67efdf13
Sourena Yadegari Varnamkhasti <sourena.yadegari@ufz.de>
2022-06-10 11:22:47 +0200

    check a copy of spotpy package with restart option for dds
    this is copy of head of branch dds_restart. Currently on Eve we don't
    have access to git subtree. After the maintenance (next Monday), this
    sould be added to the git installed on Eve. When ready, we will
    replace this copy with a subtree of the same branch.

---
+++ spotpy/setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2015, Tobias Houska
+
+from setuptools import setup, find_packages
+import os
+
+setup(
+  name = 'spotpy',
+  version = '1.5.14',
+  description = 'A Statistical Parameter Optimization Tool',
+  long_description=open(os.path.join(os.path.dirname(__file__),
+                                       "README.rst")).read(),
+  author = 'Tobias Houska, Philipp Kraft, Alejandro Chamorro-Chavez and Lutz Breuer',
+  author_email = 'tobias.houska@umwelt.uni-giessen.de',
+  url = 'https://spotpy.readthedocs.io/en/latest/',
+  license = 'MIT',
+  install_requires=[
+          'scipy'],
+  packages=find_packages(exclude=["tests*", "docs*"]),
+  use_2to3 = True,
+  keywords = 'Monte Carlo, MCMC, MLE, SCE-UA, Simulated Annealing, DE-MCz, DREAM, ROPE, Artifical Bee Colony, DDS, PA-DDS, NSGAii, Uncertainty, Calibration, Model, Signatures',
+  classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+        )
